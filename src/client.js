9d1d53afa40b2f59458483be336d719cac8214ed
Alex Newman <alex02new@outlook.com>
2025-07-30 18:05:28 -0400

    Fix.

--- src/client.js
+++ src/client.js
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js';
 
 const URL = 'https://zeolbgcvmunhqydwgbna.supabase.co';
-const API_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Inplb2xiZ2N2bXVuaHF5ZHdnYm5hIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTM0OTQ0ODUsImV4cCI6MjA2OTA3MDQ4NX0.7ifavNuXtW1WaWdCcQYt7oWkMH4gkyB0jAKb8WqN2gk';
+const API_KEY = import.meta.env.VITE_API_KEY;
 
 export const supabase = createClient(URL, API_KEY);
